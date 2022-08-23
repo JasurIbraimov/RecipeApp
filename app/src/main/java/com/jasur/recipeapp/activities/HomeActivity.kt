624f391ece9509587bd5ff13e5bb72b451169336
Jasur <ibraimov0409@mail.ru>
2022-08-23 10:16:00 +0600

    Added Recycler View Item (rvi) for main category

--- app/src/main/java/com/jasur/recipeapp/activities/HomeActivity.kt
+++ app/src/main/java/com/jasur/recipeapp/activities/HomeActivity.kt
@@ -2,11 +2,17 @@ package com.jasur.recipeapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.jasur.recipeapp.R
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val mainCategoryList: RecyclerView = findViewById(R.id.main_category_list)
+        mainCategoryList.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+
     }
 }
